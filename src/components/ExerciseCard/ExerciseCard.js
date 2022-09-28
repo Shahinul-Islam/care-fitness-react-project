ece42ce80cc9dc97cc476d0e3e3e1134e95c7fe1
shahinul islam <shahinul37@outlook.com>
2022-09-29 00:50:55 +0600

    card created

--- src/components/ExerciseCard/ExerciseCard.js
+++ src/components/ExerciseCard/ExerciseCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const ExerciseCard = ({data}) => {
-    const {picture, name} = data
+    const {picture, name, age, time} = data
     console.log(data)
     return (
         <div className='bg-slate-400 p-6 rounded'>
             <img src={picture} className="bg-center rounded w-full h-36"></img>
             <h5>{name}</h5>
+            <p>Age:{age}</p>
+            <p>Time Required: {time}s</p>
         </div>
     );
 };
